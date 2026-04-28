3e08cc7c3444c40ff02add0295bfc6138203158b
elsantos121 <santys1629@gmail.com>
2026-04-28 20:09:57 -0300

    commit1.4

--- trabajo.ts
+++ trabajo.ts
@@ -1,142 +1,40 @@
-class Turbina
-{
-    private  numTurbinas:number = 0;
-    public constructor( n :number) 
-    {
-        this.numTurbinas = n;
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+let aeroplanos: any[] = [];
+
+// CREATE
+app.post("/aeroplanos", (req, res) => {
+    const nuevo = { id: Date.now(), ...req.body };
+    aeroplanos.push(nuevo);
+    res.json(nuevo);
+});
+
+// READ
+app.get("/aeroplanos", (req, res) => {
+    res.json(aeroplanos);
+});
+
+// UPDATE
+app.put("/aeroplanos/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const index = aeroplanos.findIndex(a => a.id === id);
+
+    if (index !== -1) {
+        aeroplanos[index] = { ...aeroplanos[index], ...req.body };
+        res.json(aeroplanos[index]);
+    } else {
+        res.status(404).send("No encontrado");
     }
-    public  ToString()
-    {
-        return this.numTurbinas + " Turbina/s";
-    }
-}
+});
 
-class Helice
-{
-    private  numHelices:number = 0;
-    public constructor( n:number)
-    {
-        this.numHelices = n;
-    }
-    public ToString()
-    {
-        return this.numHelices + " hélice/s";
-}
+// DELETE
+app.delete("/aeroplanos/:id", (req, res) => {
+    const id = Number(req.params.id);
+    aeroplanos = aeroplanos.filter(a => a.id !== id);
+    res.send("Eliminado");
+});
 
-}
-
-class TrendeAterrizaje
-{
-    private  numNeumaticos:number = 0;
-    private  numAmortiguadores:number= 0;
-    private  fijoRetractil:boolean = false;
-    public constructor(a:number, b:number, c:boolean)
-    {
-        this.numNeumaticos = a;
-        this.numAmortiguadores = b;
-        this.fijoRetractil = c;
-    }
-    public  ToString()
-    {
-        let mensaje:string = "Tren de Aterrizaje compuesto por: ";
-        if (this.fijoRetractil)
-        {
-            mensaje += " con Retractil fijo, ";
-        }
-        mensaje +=  this.numNeumaticos + " neumáticos, " + this.numAmortiguadores + " amoriguadores ";
-        return mensaje;
-    }
-}
-
-class Cubierta
-{
-    private cabinaTripulacion:boolean = false;
-    private cabinaVuelo:boolean = false;
-    private sistemaEmergencia:boolean = false;
-    private numTanquesCombustible:number = 0;
-    private numPuertasSalidas:number = 0;
-    public constructor( pCabinaTripulacion:boolean,  pCabinaVuelo:boolean,  pSistemaEmergencia:boolean,  pTanquesCombustible:number,  pPuertasSalida:number)
-    {
-        this.cabinaTripulacion = pCabinaTripulacion;
-        this.cabinaVuelo = pCabinaVuelo;
-        this.sistemaEmergencia = pSistemaEmergencia;
-        this.numTanquesCombustible = pTanquesCombustible;
-        this.numPuertasSalidas = pPuertasSalida;
-    }
-
-    public ToString()
-    {
-        let mensaje = "Cubierta compuesta de: ";
-        if (this.cabinaVuelo)
-        {
-            mensaje += " Cubierta de Vuelo, ";
-        }
-        if (this.cabinaTripulacion)
-        {
-            mensaje += " Cubierta de Tripulación, ";
-        }
-        if (this.sistemaEmergencia)
-        {
-            mensaje += " Sistema de Emergencia, ";
-        }
-        mensaje += this.numTanquesCombustible + " Tanques de Combustible, ";
-        mensaje += this.numPuertasSalidas + " Puertas de Salida.";
-        return mensaje;
-    }
-}
-
-class Alas
-{
-   private  numAlasFrente:number = 0;
-   private  numAlasCola:number = 0;
-   public constructor( mAlasFrente:number,  nAlasCola:number) 
-   {
-    this.numAlasFrente = mAlasFrente;
-    this.numAlasCola = nAlasCola;
-   }
-
-    public ToString()
-    {
-        return "Alas Frontales: " + this.numAlasFrente + " Alas Posteriore: " + this.numAlasCola;
-    }
-
-}
-
-class Aeroplano
-{
-    private  helice: Helice ;
-    private  trenAterrizaje:TrendeAterrizaje;
-    private  alas: Alas ;
-    private  cubierta:Cubierta ;
-
-    constructor( phelice:Helice,  pTrenAterrizaje:TrendeAterrizaje,  pAlas:Alas,  pCubierta:Cubierta)
-    {
-         this.helice = phelice;
-         this.trenAterrizaje = pTrenAterrizaje;
-         this.alas = pAlas;
-         this.cubierta = pCubierta;
-    }
-    public  ToString()
-    {
-        let mensaje = "Aeroplano compuesto por: ";
-        mensaje += this.helice.ToString();
-        mensaje += this.alas.ToString();
-        mensaje += this.trenAterrizaje.ToString();
-        mensaje += this.cubierta.ToString();
-        return mensaje;
-
-    }
-}
-
-let  helice: Helice = new Helice(3);
-let  trenAterrizaje:TrendeAterrizaje = new TrendeAterrizaje(2,3,true);
-let  alas: Alas = new Alas(2,3);
-let  cubierta:Cubierta = new Cubierta(true,true,true,4,4);
-
-let aeroplano=new Aeroplano(helice, trenAterrizaje, alas, cubierta);
-console.log(aeroplano.ToString());
-//create
-function createAeroplano( phelice:Helice,  pTrenAterrizaje:TrendeAterrizaje,  pAlas:Alas,  pCubierta:Cubierta): Aeroplano {
-    const aeroplano = new Aeroplano(phelice, pTrenAterrizaje, pAlas, pCubierta);
-    return aeroplano;
-}
+app.listen(3000, () => console.log("Backend en puerto 3000"));
