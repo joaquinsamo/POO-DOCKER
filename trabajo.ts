e7c06c43673cc3ae1ea128a7b4087ef0a888c559
joaquinsamo <joaquinsamo0101@gmail.com>
2026-05-04 19:29:52 -0300

    commit1.5

--- trabajo.ts
+++ trabajo.ts
@@ -1,40 +1,121 @@
-import express from "express";
+class Turbina {
+    private numTurbinas:number = 0;
 
-const app = express();
-app.use(express.json());
-
-let aeroplanos: any[] = [];
-
-// CREATE
-app.post("/aeroplanos", (req, res) => {
-    const nuevo = { id: Date.now(), ...req.body };
-    aeroplanos.push(nuevo);
-    res.json(nuevo);
-});
-
-// READ
-app.get("/aeroplanos", (req, res) => {
-    res.json(aeroplanos);
-});
-
-// UPDATE
-app.put("/aeroplanos/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const index = aeroplanos.findIndex(a => a.id === id);
-
-    if (index !== -1) {
-        aeroplanos[index] = { ...aeroplanos[index], ...req.body };
-        res.json(aeroplanos[index]);
-    } else {
-        res.status(404).send("No encontrado");
+    public constructor(n:number) {
+        this.numTurbinas = n;
     }
-});
 
-// DELETE
-app.delete("/aeroplanos/:id", (req, res) => {
-    const id = Number(req.params.id);
-    aeroplanos = aeroplanos.filter(a => a.id !== id);
-    res.send("Eliminado");
-});
+    public ToString() {
+        return this.numTurbinas + " Turbina/s";
+    }
+}
 
-app.listen(3000, () => console.log("Backend en puerto 3000"));
+class Helice {
+    private numHelices:number = 0;
+
+    public constructor(n:number) {
+        this.numHelices = n;
+    }
+
+    public ToString() {
+        return this.numHelices + " hélice/s ";
+    }
+}
+
+// 🔴 COMPOSICIÓN
+class TrendeAterrizaje {
+    private numNeumaticos:number = 0;
+    private numAmortiguadores:number= 0;
+    private fijoRetractil:boolean = false;
+
+    public constructor(a:number, b:number, c:boolean) {
+        this.numNeumaticos = a;
+        this.numAmortiguadores = b;
+        this.fijoRetractil = c;
+    }
+
+    public ToString() {
+        let mensaje = "Tren de Aterrizaje: ";
+        if (this.fijoRetractil) mensaje += "Retráctil, ";
+        mensaje += this.numNeumaticos + " neumáticos, " + this.numAmortiguadores + " amortiguadores. ";
+        return mensaje;
+    }
+}
+
+// 🔴 COMPOSICIÓN
+class Cubierta {
+    private cabinaTripulacion:boolean;
+    private cabinaVuelo:boolean;
+    private sistemaEmergencia:boolean;
+    private numTanquesCombustible:number;
+    private numPuertasSalidas:number;
+
+    public constructor(a:boolean,b:boolean,c:boolean,d:number,e:number) {
+        this.cabinaTripulacion = a;
+        this.cabinaVuelo = b;
+        this.sistemaEmergencia = c;
+        this.numTanquesCombustible = d;
+        this.numPuertasSalidas = e;
+    }
+
+    public ToString() {
+        return `Cubierta: ${this.numTanquesCombustible} tanques, ${this.numPuertasSalidas} puertas. `;
+    }
+}
+
+// 🟡 AGREGACIÓN
+class Alas {
+    private numAlasFrente:number;
+    private numAlasCola:number;
+
+    public constructor(a:number, b:number) {
+        this.numAlasFrente = a;
+        this.numAlasCola = b;
+    }
+
+    public ToString() {
+        return `Alas: ${this.numAlasFrente} frontales, ${this.numAlasCola} traseras. `;
+    }
+}
+
+// 🟡 AGREGACIÓN
+class Aeroplano {
+
+    // Agregación
+    private helice?: Helice;
+    private alas?: Alas;
+
+    // Composición
+    private trenAterrizaje: TrendeAterrizaje;
+    private cubierta: Cubierta;
+
+    constructor(
+        tren:TrendeAterrizaje,
+        cubierta:Cubierta,
+        helice?:Helice,
+        alas?:Alas
+    ){
+        this.trenAterrizaje = tren;
+        this.cubierta = cubierta;
+        this.helice = helice;
+        this.alas = alas;
+    }
+
+    public ToString() {
+        let mensaje = "Aeroplano: ";
+        if(this.helice) mensaje += this.helice.ToString();
+        if(this.alas) mensaje += this.alas.ToString();
+        mensaje += this.trenAterrizaje.ToString();
+        mensaje += this.cubierta.ToString();
+        return mensaje;
+    }
+}
+
+// TEST
+let helice = new Helice(3);
+let tren = new TrendeAterrizaje(2,3,true);
+let alas = new Alas(2,3);
+let cubierta = new Cubierta(true,true,true,4,4);
+
+let aero = new Aeroplano(tren, cubierta, helice, alas);
+console.log(aero.ToString());
